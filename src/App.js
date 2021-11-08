f8661e07d4b8371f03441ef82e7407e0a20200e7
Aman Kumar Singh <aman.singh@theporter.in>
2021-11-08 17:21:34 +0530

    fetching in worker

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React from "react";
-import axios from "axios";
+// import axios from "axios";
 
 import { ExportToExcel } from "./ExportToExcel";
 
 function App() {
-  const [data, setData] = React.useState([]);
   const fileName = "myfile"; // here enter filename for your excel file
 
-  React.useEffect(() => {
-    const fetchData = () => {
-      axios
-        .get(
-          "https://api.bseindia.com/BseIndiaAPI/api/MktRGainerLoserData/w?GLtype=gainer&IndxGrp=AllMkt&IndxGrpval=AllMkt&orderby=all"
-        )
-        .then((r) => setData(r.data.Table));
-    };
-    fetchData();
-  }, []);
   let [str, setStr] = React.useState("");
   return (
     <div className="App">
-      <ExportToExcel apiData={data} fileName={fileName} />
+      <ExportToExcel fileName={fileName} />
       <input type="text" value={str} onChange={(e) => setStr(e.target.value)} />
     </div>
   );
--- src/ExportToExcelWorker.js
+++ src/ExportToExcelWorker.js
@@ -1,21 +1,31 @@
 import * as XLSX from "xlsx";
+import axios from "axios";
 
-const fileType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
+const fileType =
+  "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
 
-const exportToCSVBlob = (apiData, fileName) => {
-  const ws = XLSX.utils.json_to_sheet(apiData);
-  const wb = { Sheets: { data: ws }, SheetNames: ["data"] };
-  const excelBuffer = XLSX.write(wb, { bookType: "xlsx", type: "array" });
-  const blob = new Blob([excelBuffer], { type: fileType });
-  return blob;
-};
+const exportToCSVBlob = () => 
+  axios
+    .get(
+      "https://api.bseindia.com/BseIndiaAPI/api/MktRGainerLoserData/w?GLtype=gainer&IndxGrp=AllMkt&IndxGrpval=AllMkt&orderby=all"
+    )
+    .then((r) => {
+      console.log(r);
+      const ws = XLSX.utils.json_to_sheet(r.data.Table);
+      const wb = { Sheets: { data: ws }, SheetNames: ["data"] };
+      const excelBuffer = XLSX.write(wb, { bookType: "xlsx", type: "array" });
+      const blob = new Blob([excelBuffer], { type: fileType });
+      return blob;
+    });
 
 onmessage = (e) => {
-  const { apiData, fileName } = e.data;
   const startTime = new Date().getTime();
-  const blob = exportToCSVBlob(apiData, fileName);
-  postMessage({
-    time: new Date().getTime() - startTime,
-    blob
+
+  const getBlob = exportToCSVBlob();
+  getBlob.then((bloburl) => {
+    postMessage({
+      time: new Date().getTime() - startTime,
+      blob: bloburl,
+    });
   });
 };
